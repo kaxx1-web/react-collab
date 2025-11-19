fe97635e20a83825242d22dcc962533a24218a94
kaxx1-web <kiwiryy22@gmail.com>
2025-11-19 18:12:10 -0500

    ignore fix

--- reactclient/src/socket.js
+++ reactclient/src/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
 // Replace this with your Render backend URL:
-export const socket = io("https://collaborative-drawing.onrender.com", {
+export const socket = io("https://react-collab.onrender.com", {
   transports: ["websocket"],
 });
